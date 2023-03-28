41fe312908d9c86e770acb0538a586fd624682f1
Earid A <earid777@gmail.com>
2023-03-29 00:18:40 +0600

    Create my.js

---
+++ Arrays/my.js
@@ -0,0 +1,6 @@
+// Making uppercase to array items
+let students = ['stevens', 'Lori', 'james', 'Boris', 'david', 'Oscar', 'Prima', 'dog', 'cat'],
+  lStd = [];
+for (let i = 0; i < students.length; i++) {
+  lStd.push(students[i].toLowerCase())
+};
