cd3cb4a30bcfdc2b2ecdebc7be4f6b937203ca39
Earid A <earid777@gmail.com>
2023-04-10 00:54:05 +0600

    Update my.js

--- Arrays/my.js
+++ Arrays/my.js
@@ -4,3 +4,15 @@ let students = ['stevens', 'Lori', 'james', 'Boris', 'david', 'Oscar', 'Prima', 
 for (let i = 0; i < students.length; i++) {
   lStd.push(students[i].toLowerCase())
 };
+// accessing array strings
+let str = "Do you feel lucky";
+let str2 = "";
+
+for (let val of str) {
+    if (val === 'e') {
+        str2 += val.toUpperCase();
+    } else {
+        str2 += val;
+    }
+};
+
