942116ff03318dabf38c377c2dae53a947890f45
Earid A <earid777@gmail.com>
2023-04-11 02:01:50 +0600

    Update onclick.js

--- onclick.js
+++ onclick.js
@@ -1,1 +1,15 @@
+// Hide button after click 
+<button onclick="hideBtn(this)">Click Me to Hide</button>
+function hideBtn(x) {
+    x.style.display = 'none'
+}
+// remove all Class 
+function rmvClass() {
+    let allElements = document.querySelectorAll('.text');
+    for (i = 0; allElements.length; i++) {
+        {
+            allElements[i].classList.remove('text');
+        }
+    }
 
+}
