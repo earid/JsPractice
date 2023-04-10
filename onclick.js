04f93d35bc603fd3120e2aa5bdaa93aea9780674
Earid A <earid777@gmail.com>
2023-04-11 02:02:29 +0600

    Update onclick.js

--- onclick.js
+++ onclick.js
@@ -13,3 +13,20 @@ function rmvClass() {
     }
 
 }
+
+// date calculation
+// Hide button after click 
+<button onclick="hideBtn(this)">Click Me to Hide</button>
+function hideBtn(x) {
+    x.style.display = 'none'
+}
+// remove all Class 
+function rmvClass() {
+    let allElements = document.querySelectorAll('.text');
+    for (i = 0; allElements.length; i++) {
+        {
+            allElements[i].classList.remove('text');
+        }
+    }
+
+}
