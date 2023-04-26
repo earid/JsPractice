0c6431d4943aa36f8ae1e332dd7c7d81e462e288
Earid A <earid777@gmail.com>
2023-04-27 03:04:59 +0600

    Create tableDataDisplay.js

---
+++ tableDataDisplay.js
@@ -0,0 +1,32 @@
+fetch('https://fakestoreapi.com/products/').then((data) => {
+    return data.json();
+}).then((ObjectData) => {
+    let tableData = '';
+    ObjectData.map((value) => {
+        tableData += `<tr>
+        <td>${value.title}</td>
+        <td>${value.description}</td>
+        <td>${value.price}</td>
+        <td><img src='${value.image}'><img></td>
+      </tr>`
+    })
+    document.getElementById('tBody').innerHTML = tableData
+}
+)
+
+
+<h1 class="text-center"> Data Table</h1>
+<div class="container">
+  <table class="table table-bordered">
+    <thead class="table-dark">
+      <tr>
+        <th scope="col">Title</th>
+        <th scope="col">Description</th>
+        <th scope="col">Price</th>
+        <th scope="col">Image</th>
+      </tr>
+    </thead>
+    <tbody id="tBody">
+  
+      
+    </tbody>
