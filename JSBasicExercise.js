958b332743c401eef2c1e2494651189850360eb4
Earid A <earid777@gmail.com>
2023-05-08 23:55:12 +0600

    Update JSBasicExercise.js

--- JSBasicExercise.js
+++ JSBasicExercise.js
@@ -66,3 +66,20 @@ console.log(today);
 }
   } 
 console.log(`Current Time : ${hour}${prepand} : ${minute} : ${second}`);
+// Check whether a given year is a leap year in the Gregorian calendar
+function leapyear(year) {
+  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+}
+console.log(leapyear(2016));
+console.log(leapyear(2000));
+console.log(leapyear(1700));
+console.log(leapyear(1800));
+console.log(leapyear(100));
+
+// Solution 2
+const is_leapyear = year => new Date(year, 1, 29).getMonth() === 1;
+console.log(is_leapyear(2016));
+console.log(is_leapyear(2000));
+console.log(is_leapyear(1700));
+console.log(is_leapyear(1800));
+console.log(is_leapyear(100));
